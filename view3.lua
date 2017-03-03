79ca41cb76194699a285e263bc769842459fbe5f
Bryce Skelton <mbskelton@mail.lipscomb.edu>
2017-03-03 10:37:40 -0600

    added a handler for the TH8 button.

--- view3.lua
+++ view3.lua
@@ -16,17 +16,17 @@ function scene:create( event )
 	-- INSERT code here to initialize the scene
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 
-  TH8button = display.newImageRect("TH8.png",display.contentWidth,(display.contentHeight/6))
+  TH8button = display.newImageRect("TH8.png",display.contentWidth,(display.contentHeight/6)) --creating a button
   TH8button.x = display.contentCenterX
   TH8button.y = display.contentCenterY/2
 
 	-- create a white background to fill screen
 	local background = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
-	background:setFillColor( 1 )	-- white
+background:setFillColor( 1 )
 
 	-- create some text
 	local title = display.newText( "Third View", display.contentCenterX, 125, native.systemFont, 32 )
-	title:setFillColor( 0 )	-- black
+	title:setFillColor( 0 )
 
 	local newTextParams = { text = "Base Design",
 							x = display.contentCenterX + 10,
@@ -51,12 +51,27 @@ function scene:show( event )
 	local phase = event.phase
 
 	if phase == "will" then
-		-- Called when the scene is still off screen and is about to move on screen
+-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
-		-- Called when the scene is now on screen
-		--
-		-- INSERT code here to make the scene come alive
-		-- e.g. start timers, begin animation, play audio, etc.
+-- Called when the scene is now on screen
+--
+-- INSERT code here to make the scene come alive
+-- e.g. start timers, begin animation, play audio, etc.
+local function TH8buttonHandler( event )
+  if (event.phase == "began") then
+    TH8button.xScale = 0.85
+    TH8button.yScale = 0.85
+  elseif(event.phase == "moved") then
+
+  elseif(event.phase == "ended" or event.phase == "cancelled") then
+  TH8button.xScale = 1
+  TH8button.yScale = 1
+
+  end
+  return true
+end
+TH8button:addEventListener("touch", TH8buttonHandler)
+
 	end
 end
 
