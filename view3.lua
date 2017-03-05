09c835bc3824c2b53d195919e17220fe77c3a2e2
Bryce Skelton <mbskelton@mail.lipscomb.edu>
2017-03-04 21:02:22 -0600

    added some placeholder buttons, and started work on picture array

--- view3.lua
+++ view3.lua
@@ -6,9 +6,25 @@
 
 local composer = require( "composer" )
 local scene = composer.newScene()
+
 local TH8button
 local TH8show = false
 local img
+
+local TH7button
+local TH7show = false
+
+local TH9button
+local TH9show = false
+
+local TH10button
+local TH10show = false
+
+local picarray = {}
+local swipeLeftbtn
+local swipeRightbtn
+
+
 
 function scene:create( event )
 	local sceneGroup = self.view
@@ -18,11 +34,24 @@ function scene:create( event )
 	-- INSERT code here to initialize the scene
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 
-  TH8button = display.newImageRect("TH8.png",display.contentWidth,(display.contentHeight/6)) --creating a button
+  TH8button = display.newImageRect("buttonpics/TH8.png",display.contentWidth,(display.contentHeight/20)) --creating a button
   TH8button.x = display.contentCenterX
-  TH8button.y = display.contentCenterY/2
+  TH8button.y = (display.contentCenterY * 1.75) - 40
 
-  img = display.newImageRect("base.png", 200, 200)
+  TH7button = display.newImageRect("buttonpics/TH7.png", display.contentWidth,(display.contentHeight/20))
+  TH7button.x = display.contentCenterX
+  TH7button.y = (display.contentCenterY * 1.75) - 60
+
+  TH9button = display.newImageRect("buttonpics/TH9.png",display.contentWidth,(display.contentHeight/20))
+  TH9button.x = display.contentCenterX
+  TH9button.y = (display.contentCenterY * 1.75) - 20
+
+
+  TH10button = display.newImageRect("buttonpics/TH10.png",display.contentWidth,(display.contentHeight/20))
+  TH10button.x = display.contentCenterX
+  TH10button.y = display.contentCenterY * 1.75
+
+  img = display.newImageRect("basepics/base.png", 200, 200)
   img.x = display.contentCenterX
   img.y = display.contentCenterY * 1.3
   img.isVisible = false
@@ -31,27 +60,14 @@ function scene:create( event )
 	local background = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
 background:setFillColor( 1 )
 
-	-- create some text
-	local title = display.newText( "Third View", display.contentCenterX, 125, native.systemFont, 32 )
-	title:setFillColor( 0 )
-
-	local newTextParams = { text = "Base Design",
-							x = display.contentCenterX + 10,
-							y = title.y + 215,
-							width = 310,
-							height = 310,
-							font = native.systemFont,
-							fontSize = 14,
-							align = "center" }
-	local summary = display.newText( newTextParams )
-	summary:setFillColor( 0 ) -- black
 
 	-- all objects must be added to group (e.g. self.view)
 	sceneGroup:insert( background )
-	sceneGroup:insert( title )
-	sceneGroup:insert( summary )
   sceneGroup:insert( TH8button )
   sceneGroup:insert( img )
+  sceneGroup:insert( TH7button )
+  sceneGroup:insert( TH9button )
+  sceneGroup:insert( TH10button )
 end
 
 function scene:show( event )
@@ -60,31 +76,91 @@ function scene:show( event )
 
 	if phase == "will" then
 -- Called when the scene is still off screen and is about to move on screen
+
 	elseif phase == "did" then
 -- Called when the scene is now on screen
 --
 -- INSERT code here to make the scene come alive
 -- e.g. start timers, begin animation, play audio, etc.
-local function TH8buttonHandler( event )
-  if (event.phase == "began") then
-    TH8button.xScale = 0.85
-    TH8button.yScale = 0.85
-  elseif(event.phase == "moved") then
 
-  elseif(event.phase == "ended" or event.phase == "cancelled") then
-  TH8button.xScale = 1
-  TH8button.yScale = 1
-  TH8show = true
+  local function TH8buttonHandler( event )
+    if (event.phase == "began") then
+      TH8button.xScale = 0.85
+      TH8button.yScale = 0.85
+    elseif(event.phase == "moved") then
+        TH8button.xScale = 1
+        TH8button.yScale = 1
+
+    elseif(event.phase == "ended" or event.phase == "cancelled") then
+      TH8button.xScale = 1
+      TH8button.yScale = 1
+      TH8show = true
+
+    end
+    if TH8show == true then
+      img.isVisible = true
+    end
 
   end
 
-  if TH8show == true then
-    img.isVisible = true
+  local function TH7buttonHandler( event )
+    if (event.phase == "began") then
+      TH7button.xScale = 0.85
+      TH7button.yScale = 0.85
+
+    elseif(event.phase == "moved") then
+      TH7button.xScale = 1
+      TH7button.yScale = 1
+
+    elseif(event.phase == "ended" or event.phase == "cancelled") then
+      TH7button.xScale = 1
+      TH7button.yScale = 1
+
+
+    end
   end
-end
+
+  local function TH9buttonHandler( event )
+    if (event.phase == "began") then
+      TH9button.xScale = 0.85
+      TH9button.yScale = 0.85
+
+    elseif(event.phase == "moved") then
+      TH9button.xScale = 1
+      TH9button.yScale = 1
+
+    elseif(event.phase == "ended" or event.phase == "cancelled") then
+      TH9button.xScale = 1
+      TH9button.yScale = 1
+
+    end
+  end
+
+    local function TH10buttonHandler( event )
+      if (event.phase == "began") then
+        TH10button.xScale = 0.85
+        TH10button.yScale = 0.85
+
+      elseif(event.phase == "moved") then
+        TH10button.xScale = 1
+        TH10button.yScale = 1
+
+      elseif(event.phase == "ended" or event.phase == "cancelled") then
+        TH10button.xScale = 1
+        TH10button.yScale = 1
+
+      end
+    end
+
+
 TH8button:addEventListener("touch", TH8buttonHandler)
+TH7button:addEventListener("touch", TH7buttonHandler)
+TH9button:addEventListener("touch", TH9buttonHandler)
+TH10button:addEventListener("touch", TH10buttonHandler)
 
-	end
+
+
+  end
 end
 
 function scene:hide( event )
@@ -110,6 +186,8 @@ function scene:destroy( event )
 	-- e.g. remove display objects, remove touch listeners, save state, etc.
   TH8button:removeSelf()
   TH8button = nil
+  TH7button:removeSelf()
+  TH7button = nil
 end
 
 ---------------------------------------------------------------------------------
