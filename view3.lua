3ea97a476ac7770916776bc94936a99d1d4126e8
Bryce Skelton <mbskelton@mail.lipscomb.edu>
2017-03-06 22:18:07 -0600

    made working picture carousel for TH7 images

--- view3.lua
+++ view3.lua
@@ -23,6 +23,7 @@ local picarray = {}
 local arrayindex = 0
 local swipeLeftbtn
 local swipeRightbtn
+local currentImg
 
 
 
@@ -34,9 +35,22 @@ function scene:create( event )
   --
   -- INSERT code here to initialize the scene
   -- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
-  
+
   --need to add picture carousel buttons
-  
+
+  currentImg = display.newImageRect("icon2.png", 200, 200)
+  currentImg.x = display.contentCenterX
+  currentImg.y = display.contentCenterY/2
+  currentImg.isVisible = false
+
+  swipeLeftbtn = display.newImageRect("icon2.png", 30,30)
+  swipeLeftbtn.x = 15
+  swipeLeftbtn.y = 100
+
+  swipeRightbtn = display.newImageRect("icon2.png", 30, 30)
+  swipeRightbtn.x = display.contentWidth - 15
+  swipeRightbtn.y = 100
+
   TH7button = display.newImageRect("buttonpics/TH7.png", display.contentWidth,(display.contentHeight/20))
   TH7button.x = display.contentCenterX
   TH7button.y = (display.contentCenterY * 1.75) - 60
@@ -54,8 +68,8 @@ function scene:create( event )
   TH10button = display.newImageRect("buttonpics/TH10.png",display.contentWidth,(display.contentHeight/20))
   TH10button.x = display.contentCenterX
   TH10button.y = display.contentCenterY * 1.75
-  
-  
+
+
 
   -- create a white background to fill screen
   local background = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
@@ -68,6 +82,9 @@ function scene:create( event )
   sceneGroup:insert( TH7button )
   sceneGroup:insert( TH9button )
   sceneGroup:insert( TH10button )
+  sceneGroup:insert( swipeLeftbtn )
+  sceneGroup:insert( swipeRightbtn )
+  sceneGroup:insert( currentImg )
 end
 
 function scene:show( event )
@@ -83,7 +100,7 @@ function scene:show( event )
 -- INSERT code here to make the scene come alive
 -- e.g. start timers, begin animation, play audio, etc.
 
-  local function TH7buttonHandler( event )
+    local function TH7buttonHandler( event )
       if (event.phase == "began") then
         TH7button.xScale = 0.85
         TH7button.yScale = 0.85
@@ -96,14 +113,24 @@ function scene:show( event )
         TH7button.xScale = 1
         TH7button.yScale = 1
         TH7show = true
-        
+        TH8show = false
+        TH9show = false
+        TH10show = false
+
         if TH7show == true then
-        local file = io.open("//Applications//ZeroBraneStudio.app//Contents//ZeroBraneStudio//myprograms//CoCapp//TH7basepics//TH7basenames.txt", "r")
-        for line in file:lines() do --loading the pic array with the TH7 pics
-          arrayindex = arrayindex + 1
-          picarray[arrayindex] = line
-          --print(picarray[arrayindex])
-        end
+          arrayindex = 0
+          local file = io.open("//Applications//ZeroBraneStudio.app//Contents//ZeroBraneStudio//myprograms//CoCapp//TH7basepics//TH7basenames.txt", "r")
+          local index = 0
+          for line in file:lines() do --loading the pic array with the TH7 pics
+            picarray[index] = line
+            print(picarray[index])
+            index = index + 1
+          end
+          print(picarray[0])
+          currentImg = display.newImageRect(picarray[0], 200, 200)
+          currentImg.x = display.contentCenterX
+          currentImg.y = display.contentCenterY/2
+          currentImg.isVisible = true
         end
 
 
@@ -112,6 +139,46 @@ function scene:show( event )
 
 
 -- need to add handler for picture carousel buttons
+    local function swipeLeftbtnHandler( event )
+      if (event.phase == "began") then
+
+      elseif(event.phase == "moved") then
+
+
+      elseif(event.phase == "ended" or event.phase == "cancelled") then
+        arrayindex = arrayindex - 1
+        if arrayindex == -1 then
+          arrayindex = table.getn(picarray)
+          print(arrayindex)
+        end
+
+        currentImg = display.newImageRect(picarray[arrayindex], 200, 200)
+        currentImg.x = display.contentCenterX
+        currentImg.y = display.contentCenterY/2
+
+
+      end
+    end
+
+    local function swipeRightbtnHandler( event )
+      if (event.phase == "began") then
+
+      elseif(event.phase == "moved") then
+
+
+      elseif(event.phase == "ended" or event.phase == "cancelled") then
+        arrayindex = arrayindex + 1
+        if arrayindex == (table.getn(picarray) + 1) then
+          arrayindex = 0
+          print(arrayindex)
+        end
+        currentImg = display.newImageRect(picarray[arrayindex], 200, 200)
+        currentImg.x = display.contentCenterX
+        currentImg.y = display.contentCenterY/2
+
+      end
+    end
+
     local function TH8buttonHandler( event )
       if (event.phase == "began") then
         TH8button.xScale = 0.85
@@ -166,6 +233,8 @@ function scene:show( event )
     TH7button:addEventListener("touch", TH7buttonHandler)
     TH9button:addEventListener("touch", TH9buttonHandler)
     TH10button:addEventListener("touch", TH10buttonHandler)
+    swipeLeftbtn:addEventListener("touch", swipeLeftbtnHandler)
+    swipeRightbtn:addEventListener("touch", swipeRightbtnHandler)
 
 
 
